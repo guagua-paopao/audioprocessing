7d34fc43c11380c1f9d0f9a22b93112867258d27
fefelixa <nbd23qeu@uea.ac.uk>
2025-10-19 16:09:56 +0100

    update speech recogniser

--- speech_collector.py
+++ speech_collector.py
@@ -5,6 +5,7 @@ import soundfile as sf
 
 name_file = open("NAMES.txt")
 names = [name.strip() for name in name_file.readlines()]
+names_dict = dict.fromkeys(names, 0)
 name_file.close()
 names_dict = dict()
 for n in names:
@@ -12,46 +13,53 @@ for n in names:
 
 TARGET = 20
 
-#
+# count existing recordings
 for filename in os.listdir("recordings/"):
     if filename[:-7].title() in names and filename[-4:] == ".wav":
         name = filename[:-7].title()
         names_dict[name] += 1
 
+# remove any completed names
 for k, v in names_dict.items():
-    print(f"{v:>3} {k}")
     if v >= TARGET:
         names.remove(k)
-
-
+        print(f' ✅  {k}')
+    else:
+        print(f"{v:>3} {k}")
 
 
 FS = 16000
 SECONDS = 3
-running = (len(names) > 0)
+
+running = len(names) > 0
 
 while running:
-    name = names[randint(0, len(names) - 1)]
+    name = names[randint(0, len(names) - 1)]  # get a random name
     while names_dict[name] >= TARGET and len(names) > 0:
-        names_dict.pop(name)
-        names.remove(name)
-        name = names[randint(0, len(names) - 1)]
+        names.remove(name)  # remove any completed names
+        print(f'{name} is completed')
+        try:
+            name = names[randint(0, len(names) - 1)]
+        except:
+            print('cant choose a name')
     if len(names) == 0:
+        print('All names complete')
         break
     cmd = input(f"{name} ({names_dict[name]}/{TARGET})").strip()
-    if cmd == "" or cmd == "p":
+    if cmd == "" or cmd == "p":  # enter to record, 'p' to play recording after
         # record
         r = sd.rec(SECONDS * FS, samplerate=FS, channels=1)
         print(f"RECORDING...")
         sd.wait()
-        r_norm = 0.99 * r / max(abs(r))
 
         # save recording "name123.wav"
+        r_norm = 0.99 * r / max(abs(r))
         out_name = f"recordings/{name.lower()}{names_dict[name] + 1:03}.wav"
         sf.write(out_name, r_norm, FS)
-        if cmd == "p":
-            print("PLAYING...")
+        if cmd == "p":  # play recording
             sd.play(r_norm, FS)
+            print("PLAYING...")
+            sd.wait()
         names_dict[name] += 1
     else:
         running = False
