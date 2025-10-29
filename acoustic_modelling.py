631bc9ab832b2f52c6ff4a4c30ce27e6535c069d
fefelixa <nbd23qeu@uea.ac.uk>
2025-10-29 13:48:29 +0000

    added machine learning

--- acoustic_modelling.py
+++ acoustic_modelling.py
@@ -1,18 +1,33 @@
-import glob
-import os
-from pathlib import Path
-
-import sounddevice as sd
 import soundfile as sf
+import sounddevice as sd
+
+import matplotlib.pyplot as plt
 import numpy as np
+
+import glob
+from pathlib import Path
+import os
+
+from keras.src.metrics.metrics_utils import confusion_matrix
+from keras.utils import to_categorical
+from sklearn.preprocessing import LabelEncoder
+
+from sklearn.model_selection import train_test_split
+
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten, Conv2D, InputLayer, MaxPooling2D
+from keras.optimizers import Adam
+import features
+from sklearn import metrics
 
 name_file = open("NAMES.txt")
 names = [name.strip().lower() for name in name_file.readlines()]
 
 data = []
 labels = []
-MAX_FRAMES = 94
-for mfcc_file in sorted(glob.glob("features/mfccs/*.npy")):
+MAX_FRAMES = 298
+for mfcc_file in sorted(glob.glob("data/features/audio/*.npy")):
     mfcc_data = np.load(mfcc_file)
     mfcc_data = np.pad(mfcc_data, ((0, 0), (0, MAX_FRAMES - mfcc_data.shape[1])))
     data.append(mfcc_data)
@@ -21,4 +36,45 @@ for mfcc_file in sorted(glob.glob("features/mfccs/*.npy")):
     labels.append(label)
 labels = np.array(labels)
 data =np.array(data)
-data = data
+data = data/max(abs(np.min(data)),np.max(data)) # keep the data between 0 and 1
+
+LE = LabelEncoder()
+LE.fit(names)
+labels=to_categorical(LE.transform(labels))
+
+X_train, X_tmp, y_train,y_tmp=train_test_split(data,labels,test_size=0.2,random_state=0)
+X_val, X_test, y_val, y_test = train_test_split(X_tmp, y_tmp, test_size=0.5, random_state=0)
+
+def create_model():
+    num_classes = 20
+    model = Sequential()
+    model.add(InputLayer(input_shape=(42, 298, 1)))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(3, 3)))
+    model.add(Flatten())
+    model.add(Dense(256))
+    model.add(Activation('relu'))
+    model.add(Dense(num_classes))
+    model.add(Activation('softmax'))
+    return model
+
+
+
+model = create_model()
+model.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=0.01), metrics=['accuracy'])
+model.summary()
+
+EPOCHS = 25
+BATCH_SIZE = 32
+history = model.fit(X_train,y_train,validation_data=(X_val,y_val),epochs=EPOCHS,batch_size=BATCH_SIZE,verbose=1)
+model.save_weights('model.weights.h5')
+
+test_rec = sd.rec(3*16000, samplerate=16000, channels=1)
+sf.write('data/test/test.wav', test_rec, 16000)
+test_mfcc = features.wav_to_mfcc("data/test/test.wav")
+
+predicted_prob=model.predict(X_test,verbose=0)
+predicted = np.argmax(predicted_prob, axis=1)
+actual = np.argmax(y_test,axis=1)
+accuracy = metrics.accuracy_score(actual, predicted)
+print(accuracy)
